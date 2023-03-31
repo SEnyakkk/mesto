7d55f63aef40af309b88cf94de5f2cb5e26195c8
Sen <Senstorage@yandex.ru>
2023-03-31 12:01:19 +0300

    fix: add batton

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,3 @@
-/* // попап для профиля
-const editButton = document.querySelector('.profile__edit-button');
-const popupElement = document.querySelector('.popup');
-const closeButton = popupElement.querySelector('.popup__close-button');
-const profileInfo = document.querySelector('.profile__info');
-const infoTitle = profileInfo.querySelector('.profile__info-title');
-const infoSubtitle = profileInfo.querySelector('.profile__info-subtitle');
-const userData = document.querySelector('.form');
-const inputTitle = userData.querySelector('.form__data_user_name');
-const inputSubtitle = userData.querySelector('.form__data_user_job');
-
-const popupOpened = function () {
-  popupElement.classList.add('popup_opened');
-  inputTitle.value = infoTitle.textContent;
-  inputSubtitle.value = infoSubtitle.textContent;
-};
-
-const popupClosed = function () {
-  popupElement.classList.remove('popup_opened');
-
-};
-
-const handleFormSubmit = function (e) {
-  e.preventDefault();
-  infoTitle.textContent = inputTitle.value;
-  infoSubtitle.textContent = inputSubtitle.value;
-  popupClosed()
-} */
-
-//галерея при открытии страницы
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -56,22 +25,77 @@ const initialCards = [
   }
 ];
 
+
+ // попап для профиля
+ const editButton = document.querySelector('.profile__edit-button');
+ const popupProfile = document.querySelector('.profile-popup');
+ const closeButton = popupProfile.querySelector('.popup__close-button');
+ const profileInfo = document.querySelector('.profile__info');
+ const infoTitle = profileInfo.querySelector('.profile__info-title');
+ const infoSubtitle = profileInfo.querySelector('.profile__info-subtitle');
+ const userData = popupProfile.querySelector('.form');
+ const inputTitle = userData.querySelector('.form__data_user_name');
+ const inputSubtitle = userData.querySelector('.form__data_user_job');
+
+ const popupOpened = function () {
+   popupProfile.classList.add('popup_opened');
+   inputTitle.value = infoTitle.textContent;
+   inputSubtitle.value = infoSubtitle.textContent;
+ };
+
+ const popupClosed = function () {
+   popupProfile.classList.remove('popup_opened');
+ };
+
+ const handleFormSubmit = function (evt) {
+  evt.preventDefault();
+  infoTitle.textContent = inputTitle.value;
+  infoSubtitle.textContent = inputSubtitle.value;
+  popupClosed()
+}
+
+//попап для галереи
+const addButton = document.querySelector('.profile__add-button');
+const popupElement = document.querySelector('.element-popup');
+const closeElButton = popupElement.querySelector('.popup__close-button');
+const userPlace = popupElement.querySelector('.form');
+const inputPlace = userPlace.querySelector('.form__data_user_place');
+const inputUrl = userPlace.querySelector('.form__data_user_url');
+
+const popupElOpened = function () {
+  popupElement.classList.add('popup_opened');
+
+};
+
+const popupElClosed = function () {
+  popupElement.classList.remove('popup_opened');
+};
+
+const handleElFormSubmit = function (evt) {
+  evt.preventDefault();
+  place = inputPlace.value;
+  url = inputUrl.value;
+  elementsList.prepend(createCard (place, url));
+  popupElClosed()
+}
+
+
+//галерея при открытии страницы
 const elementTemplate = document.querySelector('.element-template').content;
 const elementsList = document.querySelector('.elements__list');
 const elementDelete = elementTemplate.querySelector('.element__delete')
 
-initialCards.forEach(createCard);
+//отрисовква элементов
+initialCards.forEach(card => (createCard(card.name, card.link)));
 
 function createCard (name, link) {
   const card = elementTemplate.querySelector('.element').cloneNode(true);
   const cardImage = card.querySelector('.element__image');
+  card.querySelector('.element__text').textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
-  card.querySelector('.element__text').textContent = name;
-  card.querySelector('.element__image').src = link;
   setEventListeners(card)
   elementsList.append(card)
-
   return card
 }
 
@@ -86,13 +110,10 @@ function setEventListeners (card) {
 
 
 
-
-//initialCards.forEach(element => elementsItem.prepend(createCard(element.link, element.name)));
-
-
-
 // слушатели
-/* editButton.addEventListener('click', popupOpened);
+addButton.addEventListener('click', popupElOpened);
+closeElButton.addEventListener('click', popupElClosed);
+editButton.addEventListener('click', popupOpened);
 closeButton.addEventListener('click', popupClosed);
 userData.addEventListener('submit', handleFormSubmit)
- */
+userPlace.addEventListener('submit', handleElFormSubmit)
