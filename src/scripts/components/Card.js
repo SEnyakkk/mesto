dd6328404c7b1f93ebf118ddde51fcd1e9eadb31
Senyakkk <senstorage@ya.ru>
2023-05-30 21:53:02 +0300

    add serv.popupElement

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -2,35 +2,51 @@ export class Card {
   constructor(data, templateSelector, openZoomPopup, openPopupDelet) {
     this._link = data.link;
     this._name = data.name;
-    this._myId = data.myid;
-    this._ownerId = data.owner._id;
+    this._myid = data.myid;
+    this._ownerid = data.owner._id;
+    this._likes = data.likes;
+    this._likesLenght = data.likes.lenght;
     this._templateSelector = templateSelector;
     this._openZoomPopup = openZoomPopup;
     this._openPopupDelet = openPopupDelet;
+    // this._cardElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
+    // this._deleteElement = this._cardElement.querySelector('.element__delete');
+    // this._imageElement = this._cardElement.querySelector('.element__image');
+    // this._textElement = this._cardElement.querySelector('.element__text');
+    // this._likeButton = this._cardElement.querySelector('.element__group');
+    // this._counter = this.__cardElement.querySelector('.element__counter')
 
   }
 
-  _getTemplate() {
-    const tempElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
-    return tempElement;
-  }
+  // _getTemplate() {
+  //   const tempElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
+  //   return tempElement;
+  // }
 
   createCard() {
-    this._cardElement = this._getTemplate();
+    this._cardElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
     this._deleteElement = this._cardElement.querySelector('.element__delete');
     this._imageElement = this._cardElement.querySelector('.element__image');
     this._textElement = this._cardElement.querySelector('.element__text');
-    this._likeButton = this._cardElement.querySelector('.element__group');
+    this._likeButton = this._cardElement.querySelector('.element__like');
     this._imageElement.src = this._link;
     this._imageElement.alt = this._name;
     this._textElement.textContent = this._name;
     this._setEventListeners()
-    this._myTrashButton()
+    this._toggleTrashButton()
     return this._cardElement;
   }
 
-  _myTrashButton() {
-    this._myId === this._ownerId ? this._deleteElement.style.display = 'block' : this._deleteElement.style.display = 'none'
+  //отображенеи иконки удаления карточки
+  _toggleTrashButton() {
+    this._myid === this._ownerid ? this._deleteElement.style.display = 'block' : this._deleteElement.style.display = 'none'
+  }
+
+  //проверка на наличие моего лайка
+  _toggleLikeButton() {
+    this._likes.forEach(like => {
+     if (like._id === this._myid) {this._likeButton.classList.add('element__like_active')}
+    });
   }
 
   //удаление елемента
@@ -40,12 +56,13 @@ export class Card {
   };
 
   deletCard() {
+
     this._cardElement.remove();
   }
 
   //отметить элемент
   _handleLike() {
-    this._likeButton.classList.toggle('element__group_active');
+    this._likeButton.classList.toggle('element__like_active');
   };
 
   //увеличить элемент
--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -1,18 +1,12 @@
 export class Section {
-  constructor({ items, renderer }, templateSelector) {
+  constructor(renderer, templateSelector) {
     this._template = document.querySelector(templateSelector);
     // this._initialCards = items
     this._renderer = renderer
   }
 
-  // addCard() {
-  //   this._initialCards.forEach(element => {
-  //     this.addItem(element)
-  //   });
-  // }
-
   addCard(items) {
-    this._initialCards = items
+    // this._initialCards = items
     items.forEach(element => {
       this._renderer(element);
     });
