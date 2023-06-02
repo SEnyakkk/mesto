090ad26811a8a3d83179969bf3cfef9123d26935
Senyakkk <senstorage@ya.ru>
2023-06-02 16:47:04 +0300

    ревью 2

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -7,31 +7,20 @@ export class Card {
     this._likes = data.likes;
     this._likesLength = data.likes.length;
     this._handleActivLike = handleActivLike;
+    this._isLiked = false;
     this._cardid = data._id;
     this._templateSelector = templateSelector;
     this._openZoomPopup = openZoomPopup;
     this._openPopupDelet = openPopupDelet;
-    // this._cardElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
-    // this._deleteElement = this._cardElement.querySelector('.element__delete');
-    // this._imageElement = this._cardElement.querySelector('.element__image');
-    // this._textElement = this._cardElement.querySelector('.element__text');
-    // this._likeButton = this._cardElement.querySelector('.element__group');
-
-
-  }
-
-  // _getTemplate() {
-  //   const tempElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
-  //   return tempElement;
-  // }
-
-  createCard() {
     this._cardElement = document.querySelector(this._templateSelector).content.querySelector('.element').cloneNode(true);
     this._deleteElement = this._cardElement.querySelector('.element__delete');
     this._imageElement = this._cardElement.querySelector('.element__image');
     this._textElement = this._cardElement.querySelector('.element__text');
     this._likeButton = this._cardElement.querySelector('.element__like');
     this._counter = this._cardElement.querySelector('.element__counter');
+  }
+
+  createCard() {
     this._imageElement.src = this._link;
     this._imageElement.alt = this._name;
     this._textElement.textContent = this._name;
@@ -43,7 +32,7 @@ export class Card {
 
   //отметить элемент
   _handleLike() {
-    this._handleActivLike(this._likeButton, this._cardid)
+    this._handleActivLike(this._isLiked, this._cardid)
     // this._likeButton.classList.toggle('element__like_active');
   };
 
@@ -52,6 +41,7 @@ export class Card {
     this._likes.forEach(like => {
       if (like._id === this._myid) {
         this._likeButton.classList.add('element__like_active');
+        this._isLiked = true;
         return
       }
     });
@@ -60,8 +50,11 @@ export class Card {
 
   toggleLike(likes) {
     this._likeButton.classList.toggle('element__like_active');
+    this._isLiked = this._likeButton.classList.contains('element__like_active') ? true : false;
     this._counter.textContent = likes.length
   }
+
+
 
   //удаление елемента
   _handleDelete() {
--- src/scripts/components/PopupDelet.js
+++ src/scripts/components/PopupDelet.js
@@ -1,19 +1,18 @@
-import { Popup } from "./Popup.js";
+import { PopupWithForm } from "./PopupWithForm.js";
 
-export class PopupDelet extends Popup {
-  constructor(poupSelector, submitPopup) {
-    super(poupSelector);
-    this._submitPopup = submitPopup;
-    this._submitButton = this._form.querySelector('.form__save');
-    this._submitText = this._submitButton.value
+export class PopupDelet extends PopupWithForm {
+  constructor(popupSelector, submitForm) {
+    super(popupSelector);
+    this._submitForm = submitForm;
+    this._submitText = this._submitButton.textContent;
   }
 
   setEventListener() {
     super.setEventListener();
     this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
-      this._submitButton.value = (`${this._submitButton.value}...`)
-      this._submitPopup({ item: this._item, cardid: this._cardid });
+      this.renderLoading(this._submitButton.textContent, 'Да...')
+      this._submitForm({ item: this._item, cardid: this._cardid });
     })
   }
 
